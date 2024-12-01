8f22f7fe0c7d8aac24e6dd0c3f86986c9971676d
shirayuki03 <chishikikun.1027@gmail.com>
2024-12-01 22:59:53 +0900

    ホーム画面のテストUIです。

--- chatApp/ContentView.swift
+++ chatApp/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var searchText = ""
+    var myFriends: [String] = ["田中太郎", "佐藤花子"]
+    
+    var body: some View {
+        NavigationView {
+            List(self.myFriends, id: \.self) { friend in
+                NavigationLink(
+                    destination: TalkView(),
+                    label: {
+                        HStack {
+                            Image(systemName: "person.crop.circle")
+                                .font(.largeTitle)
+                            VStack {
+                                Text(friend)
+                                    .font(.headline)
+                                Text("最後の会話")
+                                    .font(.subheadline)
+                            }
+                        }
+                    })
+            }
+            .navigationTitle("トーク")
+            .searchable(text: $searchText)
+        }
+    }
+}
+
+struct TalkView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
         }
-        .padding()
     }
 }
 
