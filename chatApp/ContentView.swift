2a8cec904316ff564298941687d29250be70ee2d
shirayuki03 <chishikikun.1027@gmail.com>
2024-12-02 00:02:17 +0900

    ユーザー名入力のUIを作成しました。

--- chatApp/ContentView.swift
+++ chatApp/ContentView.swift
@@ -6,33 +6,82 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
     
+    @State private var isShowingView: Bool = false
     @State private var searchText = ""
     var myFriends: [String] = ["田中太郎", "佐藤花子"]
+    var myAccount = UserDefaults.standard
     
     var body: some View {
         NavigationView {
-            List(self.myFriends, id: \.self) { friend in
-                NavigationLink(
-                    destination: TalkView(),
-                    label: {
-                        HStack {
-                            Image(systemName: "person.crop.circle")
-                                .font(.largeTitle)
-                            VStack {
-                                Text(friend)
-                                    .font(.headline)
-                                Text("最後の会話")
-                                    .font(.subheadline)
+            VStack {
+                
+                List(self.myFriends, id: \.self) { friend in
+                    NavigationLink(
+                        destination: TalkView(),
+                        label: {
+                            HStack {
+                                Image(systemName: "person.crop.circle")
+                                    .font(.largeTitle)
+                                VStack {
+                                    Text(friend)
+                                        .font(.headline)
+                                    Text("最後の会話")
+                                        .font(.subheadline)
+                                }
+                            }
+                        })
+                }
+                .navigationTitle("トーク")
+                .searchable(text: $searchText)
+            }
+        }
+        .sheet(isPresented: $isShowingView) {
+            SetAccountView()
+        }
+        .onAppear {
+            if myAccount.string(forKey: "myAccount") == nil {
+                isShowingView = true
+            }
+        }
+    }
+}
+
+struct SetAccountView: View {
+    
+    @State private var isShowingView: Bool = true
+    @FocusState var isFocused: Bool
+    @State private var userName = ""
+    var maxUserNameLength: Int = 10
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack {
+                    Text("ユーザー名")
+                        .font(.headline)
+                        .padding()
+                    TextField("10文字以内", text: $userName)
+                        .textFieldStyle(.roundedBorder)
+                        .padding()
+                        .onReceive(Just(userName)) { _ in
+                            if userName.count > maxUserNameLength {
+                                userName = String(userName.prefix(maxUserNameLength))
                             }
                         }
-                    })
+                        .focused($isFocused)
+                }
+                Button("完了") {
+                    
+                }
+                .padding()
             }
-            .navigationTitle("トーク")
-            .searchable(text: $searchText)
+            .navigationTitle("新規登録")
         }
+        .interactiveDismissDisabled(isShowingView)
     }
 }
 
